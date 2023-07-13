7aaac7bd1d68563ca4f96b0a6c100e1b7deed63b
Dominique Hosea <dominique11h@yahoo.com>
2023-07-13 10:42:20 -0500

    fix(*): add mock data

---
+++ utils/data/faqs.js
@@ -0,0 +1,34 @@
+const faqs = [
+  {
+    id: 1,
+    question: 'Can I trial Writier before paying?',
+    answer: 'Sure! You can test out Writier on our Free plan where you can experiment with 10 000 tokens.'
+  },
+  {
+    id: 2,
+    question: 'What is a token?',
+    answer: 'For regular English text, 1 token is approximately 4 characters. As a point of reference, the collected works of Shakespeare are ~900,000 words or 1.2M tokens.'
+  },
+  {
+    id: 3,
+    question: 'Does my token quota reset each month?',
+    answer: 'Yes, your quota is reset each month based on your subscription level.'
+  },
+  {
+    id: 4,
+    question: 'What payment methods do you accept?',
+    answer: 'We accept Visa, American Express, and Mastercard credit & debit cards. All payments are securely processed by Stripe.'
+  },
+  {
+    id: 5,
+    question: 'Can I cancel my plan at any time?',
+    answer: "Your plan automatically renews at the end of each billing cycle, but you can stop it from renewing by canceling at any time. You will still pay the remainder of the term for the plan you signed up for. If you do, you'll be downgraded to the Free plan."
+  },
+  {
+    id: 6,
+    question: 'What kind of support does Writier provide?',
+    answer: 'We offer fast email support to paid accounts and prioritized help for Professional accounts. Community support will soon be available to Free accounts.'
+  }
+]
+
+export default faqs
---
+++ utils/data/plans.js
@@ -0,0 +1,25 @@
+const plans = [
+  {
+    id: 1,
+    title: 'Free',
+    subtitle: 'Try our forever free plan with acces to preemium features.',
+    price: '0',
+    tokens: '10 000'
+  },
+  {
+    id: 2,
+    title: 'Personal',
+    subtitle: 'Sentence completions for everyday contexts and personal projects.',
+    price: '9',
+    tokens: '300 000'
+  },
+  {
+    id: 3,
+    title: 'Professional',
+    subtitle: 'Extensive sentence completions for agencies and education.',
+    price: '29',
+    tokens: '1 000 000'
+  }
+]
+
+export default plans
