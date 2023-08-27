828772c133d9e42ddc1049974345bd4220be3b92
Dominique Hosea <dominique11h@yahoo.com>
2023-08-27 18:33:21 -0500

    fix(*): add auth from firebase

--- firebase.ts
+++ firebase.ts
@@ -1,18 +1,21 @@
 import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_CONFIG_API_KEY,
-  authDomain: "chatgpt-messenger-clone-ed641.firebaseapp.com",
-  projectId: "chatgpt-messenger-clone-ed641",
-  storageBucket: "chatgpt-messenger-clone-ed641.appspot.com",
-  messagingSenderId: "1061021648635",
-  appId: "1:1061021648635:web:865bda1f9907e6e77e525e",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
+
 
 // Initialize Firebase
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth();
 
-export { db };
+export { db, auth };
--- stripe/initializeStripe.ts
+++ stripe/initializeStripe.ts
@@ -4,7 +4,8 @@ let stripePromise: Stripe | null;
 
 const initializeStripe = async () => {
   if (!stripePromise) {
-    stripePromise = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_SECRET_KEY);
+    // stripePromise = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_SECRET_KEY);
+    stripePromise = await loadStripe('pk_test_51MBv0yDl9zzW61N7Ko1ZTN4JXRAR84ndYeI4pY2e1JwdqGbb8jvqUvuHukGFojCUgbs7rA35JJX3VKgWAMoWSgps00zsHqS7XL');
   }
   return stripePromise;
 };
