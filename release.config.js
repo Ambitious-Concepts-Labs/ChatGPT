5f70a06b172644ca53d172942e60a20b2cf38809
Dominique Hosea <dominique11h@yahoo.com>
2023-09-05 15:59:41 -0500

    fix(release): disable line rules

--- release.config.js
+++ release.config.js
@@ -11,7 +11,8 @@ module.exports = {
     }
   ],
   rules: {
-        'body-max-line-length': [2, 'always', 20]
+        'body-max-line-length': [0, 'always'],
+        'footer-max-line-length': [0, 'always'] 
     },
   plugins: [
     '@semantic-release/commit-analyzer',
