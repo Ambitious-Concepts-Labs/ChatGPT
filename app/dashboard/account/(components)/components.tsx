cfe3851e00c4397420116faf0d78f51f1ae12aa6
Dominique Hosea <dominique11h@yahoo.com>
2023-10-09 15:17:42 -0500

    fix(*): refactor Account component

--- app/dashboard/account/(components)/components.tsx
+++ app/dashboard/account/(components)/components.tsx
@@ -1,23 +1,24 @@
 import { useState } from "react";
 import { AiFillPlusCircle } from "react-icons/ai";
 import { BiUser } from "react-icons/bi";
+import { BsKey } from 'react-icons/bs';
 import { MdDoneAll } from "react-icons/md";
-import { updateFirebaseUser } from "../utils/firebaseHelpers";
-import Button from "./Button";
+import { LuShieldAlert } from 'react-icons/lu';
+import { updateFirebaseUser } from "../../../../utils/firebaseHelpers";
+import Button from "../../../../components/Button";
+import Toggle from '../../../../components/Toggle';
 
-export default function ProfileCard(props: any) {
+export function ProfileCard(props: any) {
   const { user, session } = props
   // Update user info in db not auth or session
   const [displayName, setName ] = useState(user?.displayName || '')
   const [email, setEmail ] = useState(user?.email || '')
   const [photoURL, setPhotoURL ] = useState(user?.photoURL || '')
-  console.log(user, 'firebase')
   const updateUser = updateFirebaseUser('any', {displayName, email, photoURL})
   console.log(updateUser)
   return (
     <div className="bg-white py-4 px-5 rounded-sm">
       <div>
-        
         <div className="pb-4 flex items-center justify-between">
           <div className="bg-slate-100 text-slate-400 p-1 h-8 w-8 flex items-center justify-center rounded-md text-xl">
             <BiUser />
@@ -76,4 +77,62 @@ export default function ProfileCard(props: any) {
       </div>
     </div>
   );
+}
+
+export function ChangePswdCard (props: any) {
+  const [password, setPassword ] = useState('')
+  return (
+    <div className='bg-white py-4 px-5 rounded-sm'>
+      <div>
+        <div className='pb-4'>
+          <div className='bg-slate-100 text-slate-400 p-1 h-8 w-8 flex items-center justify-center rounded-md text-xl'>
+            <BsKey className='-rotate-45' />
+          </div>
+        </div>
+        <h3 className='font-bold pb-7 text-xs'>Change Password</h3>
+        <form onSubmit={() => { updateFirebaseUser('password', {password}); }} className='flex flex-col gap-3 w-7/12 text-xs'>
+          <input type='text' className='p-2 border rounded-md' placeholder='Current password' />
+          <input type='text' className='p-2 border rounded-md' placeholder='New password' />
+          <input 
+          value={password}
+          onChange={(e) => { setPassword(e.target.value); }} 
+          type='text' className='p-2 border rounded-md' 
+          placeholder='Re-type new password' />
+        </form>
+      </div>
+    </div>
+  )
+}
+
+
+export function DeleteCard (props: any) {
+  const [canDelete, setCanDelete] = useState(false)
+
+  const handleCanDelete = () => {
+    setCanDelete(prev => !prev)
+  }
+
+  return (
+    <div className='lg:col-span-2 bg-white py-4 px-5 rounded-sm'>
+      <div>
+        <div className='flex items-center justify-between pb-4'>
+          <div className='bg-red-50 p-1 text-xl h-10 w-10 flex items-center justify-center rounded-md'>
+            <LuShieldAlert className='text-red-600' />
+          </div>
+          <Toggle variant='danger' handleCheck={handleCanDelete} />
+        </div>
+        <h3 className='font-bold text-xs'>Danger Zone</h3>
+        <div className='pt-8 pb-6'>
+          <h4 className='text-red-600 pb-2 text-xs'>Delete account</h4>
+          <p className='text-xs text-slate-400'>
+            Permanent delete your account and all of your content. In order to prime the sequence. click on the toggle.
+          </p>
+          <p className='text-xs text-slate-400'>
+            We also cancel any active subscriptions you may have and delete your payment information.
+          </p>
+        </div>
+        <button disabled={!canDelete} className='bg-red-600 text-white text-xs px-4 py-2 rounded-sm disabled:bg-slate-400 disabled:opacity-40'>Delete account</button>
+      </div>
+    </div>
+  )
 }
--- app/dashboard/account/page.tsx
+++ app/dashboard/account/page.tsx
@@ -1,8 +1,29 @@
 "use client";
-import Account from "../../../components/Account";
 
-const index = () => {
-  return <Account />;
-};
+import React from "react";
+import Title from "../../../components/Title";
+import { UserAuth } from "../../authContext";
+import { ChangePswdCard, DeleteCard, ProfileCard } from "./(components)/components";
 
-export default index;
+export default function Account() {
+
+  const { showModal, setShowModal, user, firebaseUser } = UserAuth();
+
+  return (
+    <div className="bg-[#f8f9fb] min-h-screen grow px-8 md:px-16 pt-7 pb-10">
+        <Title
+          showModal={showModal}
+          setShowModal={setShowModal}
+          button="Document"
+          title="Account"
+        />
+        <div
+          className="md:grid grid-cols-1 grid-rows-3 justify-self-end lg:grid-cols-2 lg:grid-rows-2 gap-5 grid"
+        >
+          <ProfileCard user={user} session={firebaseUser} />
+          <ChangePswdCard user={user} />
+          <DeleteCard user={user} />
+        </div>
+      </div>
+  );
+}
