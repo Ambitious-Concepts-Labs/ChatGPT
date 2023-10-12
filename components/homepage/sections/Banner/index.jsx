fd3f482e11c56f9853475337bbf7db6ee9f001e2
Dominique Hosea <dominique11h@yahoo.com>
2023-10-12 05:16:52 -0500

    fix(*): update signin buttons

--- components/homepage/sections/Banner/index.jsx
+++ components/homepage/sections/Banner/index.jsx
@@ -2,14 +2,14 @@
 
 import { signInWithPopup } from 'firebase/auth';
 import { RxArrowTopRight } from 'react-icons/rx'
-import { GoogleProvider } from '../../../../firebase';
+import { auth, GoogleProvider } from '../../../../firebase';
 
 export default function Banner () {
   return (
     <div className='banner p-4 pt-4 flex items-center flex-col md:flex-row md:justify-center'>
       {/* <div className='text-center text-xs text-white opacity-81'>Compete in the HackAPrompt Competition until June 3rd!</div> */}
       <div className='text-center text-xs text-white opacity-81'>Start using AC Prompt Aid today!</div>
-      <a onClick={async () => await signInWithPopup(GoogleProvider)}>
+      <a onClick={async () => await signInWithPopup(auth, GoogleProvider)}>
         <div className='flex cursor-pointer hover:underline transition-all items-center text-white font-bold tracking-tight mt-2 md:mt-0 ml-0 md:ml-3 text-sm'>
           Register Now
           <span>
--- components/homepage/shared/Button.jsx
+++ components/homepage/shared/Button.jsx
@@ -3,14 +3,14 @@
 import React from 'react';
 import { signInWithPopup } from 'firebase/auth';
 import { RxArrowTopRight } from 'react-icons/rx'
-import { GoogleProvider } from '../../../firebase';
+import { auth, GoogleProvider } from '../../../firebase';
 
 export default function Button ({ text, variant }) {
   const sharedClasses = 'group flex items-center rounded-full gap-1 justify-center px-7 py-2.5 cursor-pointer transition-all drop-shadow-md  hover:shadow-md font-medium tracking'
 
   const variantClasses = variant === 'white' ? 'font-vietnam text-[#01695D] bg-white' : 'btn text-white'
   return (
-    <div onClick={async () => await signInWithPopup(GoogleProvider)} >
+    <div onClick={async () => await signInWithPopup(auth, GoogleProvider)} >
       <div className={`${sharedClasses  } ${  variantClasses}`}>
         <div className='group-hover:underline underline-offset-2 decoration-1 decoration-white/30 tracking-tight'>{text}</div>
         <div className='font-medium text-xl flex mt-1'>
