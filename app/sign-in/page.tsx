8af6e6ca3ce307f4e5dca1be005956cc8dc8e1ad
Dominique Hosea <dominique11h@yahoo.com>
2023-10-12 05:16:10 -0500

    fix(*): implement server only componenets on signin screen

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,21 +1,12 @@
-'use client';
-
+import 'server-only';
 import { BiChevronLeft } from 'react-icons/bi'
-import { AiFillGithub } from 'react-icons/ai'
-import { FcGoogle } from 'react-icons/fc'
-import { BsDiscord } from 'react-icons/bs'
-
 import Link from 'next/link';
-import { useState } from 'react';
-import { signInWithPopup } from 'firebase/auth';
 import Button from '../../components/Button';
-import { GoogleProvider, auth } from '../../firebase';
+import { AuthSignin } from './(components)/components';
 
 
 
 export default function Page() {
-  const [email, setEmail] = useState('');
-
   return (
     <div className='grid min-h-screen grid-cols-2'>
       <div className='flex flex-col'>
@@ -32,31 +23,7 @@ export default function Page() {
               <p className='text-2xl font-medium'>Sign in</p>
               <p>Choose a social provider to sign in with.</p>
             </div>
-            <div className='flex flex-col gap-2 py-4'>
-              <Button
-                variant="black"
-                text="Sign in with Discord"
-                icon={<BsDiscord size={18} />}
-                // onClick={() => signIn('discord', { callbackUrl: '/dashboard' })}
-              />
-              <Button
-                variant="black"
-                text="Sign in with GitHub"
-                icon={<AiFillGithub size={18}/>}
-                // onClick={() => signIn('github', { callbackUrl: '/dashboard' })}
-              />
-              <Button
-                animation
-                variant="black"
-                text="Sign in with Google"
-                icon={<FcGoogle size={18}/>}
-                onClick={async () => await signInWithPopup(auth, GoogleProvider)}
-              />
-              <button onClick={async () => await signInWithPopup(auth, GoogleProvider)}
-              className="font-bold text-3xl animate-pulse">
-                Sign In to use ChatGPT
-              </button>
-            </div>
+            <AuthSignin />
           </div>
         </div>
       </div>
