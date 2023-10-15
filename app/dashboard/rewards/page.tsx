b8b263d5271d819d335c909fdacc42f96e12d2c0
Dominique Hosea <dominique11h@yahoo.com>
2023-10-15 18:47:56 -0500

    fix(*): remove client side components

--- app/dashboard/rewards/page.tsx
+++ app/dashboard/rewards/page.tsx
@@ -1,10 +1,8 @@
 import 'server-only';
 import React from "react";
-import { requireUserLoggedIn } from "../../../utils/helperFunctions";
 import { IncreaseCard, RewardsTitle, StepOneCard, StepTwoCard } from "./(components)/components";
 
 export default function Rewards() {
-  requireUserLoggedIn();   
   return (
     <>
       <RewardsTitle />
